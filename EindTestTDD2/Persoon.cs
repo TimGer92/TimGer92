5025bb5ddb0475a06e21df30b10e4654c941574c
Timmy Gerits <geritstimmy@hotmail.com>
2018-04-03 11:19:15 +0200

    Volledig

---
+++ EindTestTDD2/Persoon.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EindTestTDD2
+{
+    public class Persoon
+    {
+        private List<string> Voornamen;
+        public Persoon(List<string> voornamen)
+        {
+            this.Voornamen = voornamen;
+
+            if (voornamen.Count() == 0)
+            {
+                throw new ArgumentException();
+            }
+            if (voornamen.Contains(string.Empty))
+            {
+                throw new ArgumentException();
+            }
+            if (voornamen.Contains(" "))
+            {
+                throw new ArgumentException();
+            }
+
+            if (voornamen.Distinct().Count() != voornamen.Count())
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public override string ToString()
+        {
+            var voornamenSpaties = String.Join(" ", Voornamen);
+            return voornamenSpaties.ToString(); 
+        }
+    }
+}
---
+++ EindTestTDDTest/PersoonTest2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using EindTestTDD2;
+
+namespace EindTestTDDTest
+{
+    [TestClass]
+    public class PersoonTest
+    {
+        [TestMethod]
+        public void PersoonMoetMinstens1VoornaamBevattenControleOK()
+        {
+            new Persoon(new List<string> { "Eric" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void PersoonMoetMinstens1VoornaamBevattenControlenNietOK()
+        {
+            new Persoon(new List<string>());
+        }
+
+        [TestMethod]
+        public void PersoonKanNiet2KeerDezelfdeNaamHebbenOK()
+        {
+            new Persoon(new List<string> { "Eric", "Paul", "Albert" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void PersoonKanNiet2KeerDezelfdeNaamHebben()
+        {
+            new Persoon(new List<string> { "Eric", "Paul", "Albert", "Eric" });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void VoornaamMoetMinstens1TekenBevatten()
+        {
+            new Persoon(new List<string> { string.Empty });
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void VoornaamBevatGeenSpaties()
+        {
+            new Persoon(new List<string> { " " });
+        }
+
+        [TestMethod]
+        public void VoornaamMetMinstens1TekenOK()
+        {
+            new Persoon(new List<string> { "a" });
+        }
+
+        
+        [TestMethod]
+        public void ToStringMoetVoornamenTeruggevenMetSpaties()
+        {
+            List<string> voornamen = new List<string> () { "Eric", "Frans", "Tom" };
+            Persoon persoon = new Persoon(voornamen);
+            var output = String.Join(" ", voornamen);
+            Assert.AreEqual(output,persoon.ToString());
+        }
+        
+
+    }
+}
